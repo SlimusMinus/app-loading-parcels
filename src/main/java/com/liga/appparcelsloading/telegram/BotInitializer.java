e43758f927df43bb3ecb5c40f71c3ac8f2738fb7
SlimusMinus <moscow_city231@rambler.ru>
2024-10-07 22:30:11 +0300

    change README.md

--- src/main/java/com/liga/appparcelsloading/telegram/BotInitializer.java
+++ src/main/java/com/liga/appparcelsloading/telegram/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.liga.appparcelsloading.telegram;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
  * При возникновении события ContextRefreshedEvent регистрирует бота с использованием
  * TelegramBotsApi.
  */
+@Slf4j
 @Component
 @AllArgsConstructor
 public class BotInitializer {
@@ -27,10 +29,12 @@ public class BotInitializer {
     @EventListener({ContextRefreshedEvent.class})
     public void initialize() throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        log.info("Инициализация Telegram-бота...");
         try{
             telegramBotsApi.registerBot(telegramBot);
+            log.info("Telegram-бот успешно зарегистрирован.");
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            log.error("Ошибка при регистрации Telegram-бота: {}", e.getMessage(), e);
         }
     }
 }
